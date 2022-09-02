eafa26ffccd9f56cae53712a381afd072916db3f
liuyunan <liuyunan.20@gmail.com>
2022-09-02 00:10:48 -0700

    add fire and update logic

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,10 @@ def wiki(name="war Goddess", length=2):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    results = wikipedia.search(name)
+    return results
