564833cbbf667fa6e260d5b7623be527cab87bd0
liuyunan <liuyunan.20@gmail.com>
2022-09-02 21:45:55 -0700

    add search, phrase function in main, add test

--- main.py
+++ main.py
@@ -1,3 +1,47 @@
-from mylib.logic import wiki
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wikilogic
+from mylib.logic import phrase as wikiprases
+from fastapi import FastAPI
+import uvicorn
 
-print(wiki())
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API.  Call /search or /wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Page to search in wikipedia"""
+
+    result = wikilogic(name)
+    return {"result": result}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/phrase/{name}")
+async def phrase(name: str):
+    """Page to search in wikipedia"""
+
+    result = wikiprases(name)
+    return {"result": result}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
